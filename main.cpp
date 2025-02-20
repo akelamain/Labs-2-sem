f084aa1304b6e6ae2a6a0044d72796055ef43172
akelamain <malkovnikituschka@yandex.ru>
2025-02-20 10:27:37 +0300

    lab_1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "mymath/mymath.h"
+#include "mymath2/mymath2.h"
 
 int main() {
-    std::cout << "Hello, world!";
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Я умею суммировать! Зацени 10+41=";
+    std::cout << mymath::sum(10, 41) << std::endl;
+
+    std::cout << "Я умею умножать! Зацени 10*10=";
+    std::cout << mymath2::multiply(10, 10) << std::endl;
+    return 0;
 }
